0412411739584193d07c78abf15e87c9cb8181de
Nelson Chiu <nardmw@gmail.com>
2018-01-08 09:20:39 -0700

    2 line changes for ease of use:
    added default home route
    clarified on index.html.erb that there's only 2 datapoints in DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'sessions', to: 'sessions#index'
+  root 'sessions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
